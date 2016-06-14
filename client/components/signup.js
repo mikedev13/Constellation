1a4602851e1b734f6f58f2531383698f80dec891
Rong Huang <rkh3544@users.noreply.github.com>
2016-06-14 15:59:48 -0700

    Revert "[initial commit]"

--- client/components/signup.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/RaisedButton'
-
-export default class SignUp extends Component {
-
-}
